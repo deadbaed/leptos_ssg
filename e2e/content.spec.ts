97812eaa66f1199d2de5924d19459c62b051f666
Philippe Loctaux <p@philippeloctaux.com>
2025-07-22 20:32:54 +0200

    tests: add basic test of a page with content

---
+++ e2e/content.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("/example-site/demo");
+});
+
+test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/Demo/);
+});
+
+test("has header", async ({ page }) => {
+    expect(page.getByRole("heading", { name: "Demo of leptos_ssg" }));
+
+    // TODO: posted date
+});
+
+test("has navigation links", async ({ page }) => {
+    await expect(page.getByRole("link", { name: "Home" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Previous" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Next" })).toBeVisible();
+});
