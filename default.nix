070e43c1f4285d66688dfaa99e2c5dd3179d6bee
Philippe Loctaux <p@philippeloctaux.com>
2026-01-01 19:59:50 +0100

    nix derivation to build tailwind stylesheets

--- default.nix
+++ default.nix
@@ -1,8 +1,23 @@
 {
   sources ? import ./npins,
   pkgs ? import sources.nixpkgs { },
+  lib ? pkgs.lib,
 }:
 
+let
+  mkTailwindStylesheet =
+    name: src: minify:
+    pkgs.runCommand "${name}-compiled-tailwind"
+      {
+        inherit src;
+      }
+      ''
+        mkdir $out
+        ${pkgs.tailwindcss_4}/bin/tailwindcss --cwd $src ${lib.optionalString minify "--minify"} > $out/style.css
+      '';
+  tailwindOpengraph = "${mkTailwindStylesheet "opengraph" ./opengraph/src true}/style.css";
+  tailwindLeptosSsg = "${mkTailwindStylesheet "leptos_ssg" ./src true}/style.css";
+in
 {
   shell = pkgs.mkShellNoCC {
     packages = with pkgs; [
@@ -18,7 +33,6 @@
       nixfmt-tree
       nil
       nixfmt-rfc-style
-
     ];
 
     env = {
@@ -26,6 +40,9 @@
       # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
       # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
       RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+      CSS_OPENGRAPH = tailwindOpengraph;
+      CSS_LEPTOS_SSG = tailwindLeptosSsg;
     };
   };
 }
